dfb6879ef543d793ffa42f0a1c567253034472bd
Jacques Dafflon <jacques@bity.com>
2018-01-12 16:06:48 +0100

    Remove unused old file

--- js/ERC20CompatibleReferenceToken.js
+++
@@ -1,5 +0,0 @@
-const ERC20CompatibleReferenceTokenAbi = require('../build/ERC20CompatibleReferenceToken.sol').ERC20CompatibleReferenceTokenAbi;
-const ERC20CompatibleReferenceTokenByteCode = require('../build/ERC20CompatibleReferenceToken.sol').ERC20CompatibleReferenceTokenByteCode;
-const generateClass = require('eth-contract-class').default;
-
-module.exports = generateClass(ERC20CompatibleReferenceTokenAbi, ERC20CompatibleReferenceTokenByteCode);
