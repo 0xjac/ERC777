25c99d2c10a8e06d9ccc3ea0b8d37a4cad446a2a
Jordi Baylina <jordi@baylina.cat>
2018-01-17 19:42:43 +0100

    rename to granularity

--- contracts/Ierc777.sol
+++ contracts/Ierc777.sol
@@ -8,7 +8,7 @@ interface Ierc777 {
     function name() public constant returns (string);
     function symbol() public constant returns (string);
     function totalSupply() public constant returns (uint256);
-    function minimalUnit() public constant returns (uint256);
+    function granularity() public constant returns (uint256);
     function balanceOf(address owner) public constant returns (uint256);
 
     function send(address to, uint256 amount) public;
