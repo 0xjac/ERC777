2b39347250f5e8f0bc4b14c7b445cf76dc6a949e
Jacques Dafflon <jacques@bity.com>
2018-01-30 14:04:59 +0100

    Remove tokenable registry

--- js/TokenableContractsRegistry.js
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-const TokenableContractsRegistryAbi = require('../build/TokenableContractsRegistry.sol').TokenableContractsRegistryAbi;
-const TokenableContractsRegistryByteCode = require('../build/TokenableContractsRegistry.sol').TokenableContractsRegistryByteCode;
-const generateClass = require('eth-contract-class').default;
-
-module.exports = generateClass(TokenableContractsRegistryAbi, TokenableContractsRegistryByteCode);
--- test/ReferenceToken-test.js
+++ test/ReferenceToken-test.js
@@ -6,7 +6,6 @@ const TestRPC = require('ethereumjs-testrpc');
 const Web3 = require('web3');
 const chai = require('chai');
 const EIP820 = require('eip820');
-const TokenableContractsRegistry = require('../js/TokenableContractsRegistry');
 const ReferenceToken = require('../js/ReferenceToken');
 const assert = chai.assert;
 chai.use(require('chai-as-promised')).should();
@@ -16,7 +15,6 @@ describe('EIP777 Reference Token Test', () => {
   let web3;
   let accounts;
   let referenceToken;
-  let tokenableContractsRegistry;
   let interfaceImplementationRegistry;
   let util;
 
@@ -38,15 +36,11 @@ describe('EIP777 Reference Token Test', () => {
   after(async () => testrpc.close());
 
   it('should deploy the reference token contract', async () => {
-    tokenableContractsRegistry = await TokenableContractsRegistry.new(web3);
-    assert.ok(tokenableContractsRegistry.$address);
-
     referenceToken = await ReferenceToken.new(
       web3,
       'Reference Token',
       'XRT',
       web3.utils.toWei('0.01'),
-      tokenableContractsRegistry.$address
     );
     assert.ok(referenceToken.$address);
 
