3c9f9ae360f2c45e5e6ff852a7ab6cf5db263429
tp <mail@antontranelis.de>
2018-02-06 20:08:50 +0100

    add EIP820 tests and upgarde solcpiler to 0.0.12 so files in /js are no longer needed

--- js/ReferenceToken.js
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-const ReferenceTokenAbi = require('../build/ReferenceToken.sol').ReferenceTokenAbi;
-const ReferenceTokenByteCode = require('../build/ReferenceToken.sol').ReferenceTokenByteCode;
-const generateClass = require('eth-contract-class').default;
-
-module.exports = generateClass(ReferenceTokenAbi, ReferenceTokenByteCode);
