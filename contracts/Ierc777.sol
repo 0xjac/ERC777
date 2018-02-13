1a8e012cf5c7fd7d89381aa945e78f0cd71afd50
Jacques Dafflon <jacques@bity.com>
2018-02-13 14:41:45 +0100

    s/value/amount/g

--- contracts/Ierc777.sol
+++ contracts/Ierc777.sol
@@ -12,24 +12,24 @@ interface Ierc777 {
     function granularity() public constant returns (uint256);
     function balanceOf(address owner) public constant returns (uint256);
 
-    function send(address to, uint256 value) public;
-    function send(address to, uint256 value, bytes userData) public;
+    function send(address to, uint256 amount) public;
+    function send(address to, uint256 amount, bytes userData) public;
 
     function authorizeOperator(address operator) public;
     function revokeOperator(address operator) public;
     function isOperatorFor(address operator, address tokenHolder) public constant returns (bool);
-    function operatorSend(address from, address to, uint256 value, bytes userData, bytes operatorData) public;
+    function operatorSend(address from, address to, uint256 amount, bytes userData, bytes operatorData) public;
 
     event Sent(
         address indexed operator,
         address indexed from,
         address indexed to,
-        uint256 value,
+        uint256 amount,
         bytes userData,
         bytes operatorData
     ); // solhint-disable-next-line separate-by-one-line-in-contract
-    event Minted(address indexed operator, address indexed to, uint256 value, bytes operatorData);
-    event Burned(address indexed operator, address indexed from, uint256 value, bytes userData, bytes operatorData);
+    event Minted(address indexed operator, address indexed to, uint256 amount, bytes operatorData);
+    event Burned(address indexed operator, address indexed from, uint256 amount, bytes userData, bytes operatorData);
     event AuthorizedOperator(address indexed operator, address indexed tokenHolder);
     event RevokedOperator(address indexed operator, address indexed tokenHolder);
 }
