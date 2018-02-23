trigun0x2/eip777

erc777-tokenHolder interface with tokensToSend

contracts/ReferenceToken.sol
@@ -14,12 +14,12 @@ pragma solidity ^0.4.19; // solhint-disable-line compiler-fixed
 import "eip820/contracts/EIP820Implementer.sol";
 import "giveth-common-contracts/contracts/Owned.sol";
 import "giveth-common-contracts/contracts/SafeMath.sol";
-import "./Ierc20.sol";
-import "./Ierc777.sol";
-import "./ITokenRecipient.sol";
+import "./erc20_token.sol";
+import "./erc777_token.sol";
+import "./erc777_tokenHolder.sol";
 
 
-contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
+contract ReferenceToken is Owned, erc20_token, erc777_token, EIP820Implementer {
     using SafeMath for uint256;
 
     string private mName;
@@ -255,7 +255,7 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
     /// @param _userData Data generated by the user to be passed to the recipient
     /// @param _operatorData Data generated by the operator to be passed to the recipient
     /// @param _preventLocking `true` if you want this function to throw when tokens are sent to a contract not
-    ///  implementing `ITokenRecipient`.
+    ///  implementing `erc777_tokenHolder`.
     ///  ERC777 native Send functions MUST set this parameter to `true`, and backwards compatible ERC20 transfer
     ///  functions SHOULD set this parameter to `false`.
     function doSend(
@@ -269,6 +269,9 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
     )
         private
     {
+
+        callSender(_operator, _from, _to, _value, _userData, _operatorData, _preventLocking);
+
         requireMultiple(_value);
         require(_to != address(0));          // forbid sending to 0x0 (=burning)
         require(mBalances[_from] >= _value); // ensure enough funds
@@ -282,7 +285,7 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
         if (mErc20compatible) { Transfer(_from, _to, _value); }
     }
 
-    /// @notice Helper function that checks for ITokenRecipient on the recipient and calls it.
+    /// @notice Helper function that checks for erc777_tokenHolder on the recipient and calls it.
     ///  May throw according to `_preventLocking`
     /// @param _from The address holding the tokens being sent
     /// @param _to The address of the recipient
@@ -290,7 +293,7 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
     /// @param _userData Data generated by the user to be passed to the recipient
     /// @param _operatorData Data generated by the operator to be passed to the recipient
     /// @param _preventLocking `true` if you want this function to throw when tokens are sent to a contract not
-    ///  implementing `ITokenRecipient`.
+    ///  implementing `erc777_tokenHolder`.
     ///  ERC777 native Send functions MUST set this parameter to `true`, and backwards compatible ERC20 transfer
     ///  functions SHOULD set this parameter to `false`.
     function callRecipient(
@@ -302,12 +305,41 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
         bytes _operatorData,
         bool _preventLocking
     ) private {
-        address recipientImplementation = interfaceAddr(_to, "ITokenRecipient");
+        address recipientImplementation = interfaceAddr(_to, "erc777_tokenHolder");
         if (recipientImplementation != 0) {
-            ITokenRecipient(recipientImplementation).tokensReceived(
+            erc777_tokenHolder(recipientImplementation).tokensReceived(
                 _operator, _from, _to, _value, _userData, _operatorData);
         } else if (_preventLocking) {
             require(isRegularAddress(_to));
+        }
+    }
+
+    /// @notice Helper function that checks for erc777_tokenHolder on the sender and calls it.
+    ///  May throw according to `_preventLocking`
+    /// @param _from The address holding the tokens being sent
+    /// @param _to The address of the recipient
+    /// @param _value The amount of tokens to be sent
+    /// @param _userData Data generated by the user to be passed to the recipient
+    /// @param _operatorData Data generated by the operator to be passed to the recipient
+    /// @param _preventLocking `true` if you want this function to throw when tokens are sent from a contract not
+    ///  implementing `erc777_tokenHolder`.
+    ///  ERC777 native Send functions MUST set this parameter to `true`, and backwards compatible ERC20 transfer
+    ///  functions SHOULD set this parameter to `false`.
+    function callSender(
+        address _operator,
+        address _from,
+        address _to,
+        uint256 _value,
+        bytes _userData,
+        bytes _operatorData,
+        bool _preventLocking
+    ) private {
+        address senderImplementation = interfaceAddr(_from, "erc777_tokenHolder");
+        if (senderImplementation != 0) {
+            erc777_tokenHolder(senderImplementation).tokensToSend(
+                _operator, _from, _to, _value, _userData, _operatorData);
+        } else if (_preventLocking) {
+            require(isRegularAddress(_from));
         }
     }
 }

0xjac/ERC777

contracts/ReferenceToken.sol
@@ -14,12 +14,12 @@ pragma solidity ^0.4.19; // solhint-disable-line compiler-fixed
 import "eip820/contracts/EIP820Implementer.sol";
 import "giveth-common-contracts/contracts/Owned.sol";
 import "giveth-common-contracts/contracts/SafeMath.sol";
-import "./Ierc20.sol";
-import "./Ierc777.sol";
-import "./ITokenRecipient.sol";
+import "./erc20_token.sol";
+import "./erc777_token.sol";
+import "./erc777_tokenHolder.sol";
 
 
-contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
+contract ReferenceToken is Owned, erc20_token, erc777_token, EIP820Implementer {
     using SafeMath for uint256;
 
     string private mName;
@@ -255,7 +255,7 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
     /// @param _userData Data generated by the user to be passed to the recipient
     /// @param _operatorData Data generated by the operator to be passed to the recipient
     /// @param _preventLocking `true` if you want this function to throw when tokens are sent to a contract not
-    ///  implementing `ITokenRecipient`.
+    ///  implementing `erc777_tokenHolder`.
     ///  ERC777 native Send functions MUST set this parameter to `true`, and backwards compatible ERC20 transfer
     ///  functions SHOULD set this parameter to `false`.
     function doSend(
@@ -270,6 +270,9 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
         private
     {
         requireMultiple(_amount);
+
+        callSender(_operator, _from, _to, _value, _userData, _operatorData, _preventLocking);
+
         require(_to != address(0));          // forbid sending to 0x0 (=burning)
         require(mBalances[_from] >= _amount); // ensure enough funds
 
@@ -282,7 +285,7 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
         if (mErc20compatible) { Transfer(_from, _to, _amount); }
     }
 
-    /// @notice Helper function that checks for ITokenRecipient on the recipient and calls it.
+    /// @notice Helper function that checks for erc777_tokenHolder on the recipient and calls it.
     ///  May throw according to `_preventLocking`
     /// @param _from The address holding the tokens being sent
     /// @param _to The address of the recipient
@@ -290,7 +293,7 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
     /// @param _userData Data generated by the user to be passed to the recipient
     /// @param _operatorData Data generated by the operator to be passed to the recipient
     /// @param _preventLocking `true` if you want this function to throw when tokens are sent to a contract not
-    ///  implementing `ITokenRecipient`.
+    ///  implementing `erc777_tokenHolder`.
     ///  ERC777 native Send functions MUST set this parameter to `true`, and backwards compatible ERC20 transfer
     ///  functions SHOULD set this parameter to `false`.
     function callRecipient(
@@ -302,12 +305,41 @@ contract ReferenceToken is Owned, Ierc20, Ierc777, EIP820Implementer {
         bytes _operatorData,
         bool _preventLocking
     ) private {
-        address recipientImplementation = interfaceAddr(_to, "ITokenRecipient");
+        address recipientImplementation = interfaceAddr(_to, "erc777_tokenHolder");
         if (recipientImplementation != 0) {
-            ITokenRecipient(recipientImplementation).tokensReceived(
+            erc777_tokenHolder(recipientImplementation).tokensReceived(
                 _operator, _from, _to, _amount, _userData, _operatorData);
         } else if (_preventLocking) {
             require(isRegularAddress(_to));
+        }
+    }
+
+    /// @notice Helper function that checks for erc777_tokenHolder on the sender and calls it.
+    ///  May throw according to `_preventLocking`
+    /// @param _from The address holding the tokens being sent
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be sent
+    /// @param _userData Data generated by the user to be passed to the recipient
+    /// @param _operatorData Data generated by the operator to be passed to the recipient
+    /// @param _preventLocking `true` if you want this function to throw when tokens are sent from a contract not
+    ///  implementing `erc777_tokenHolder`.
+    ///  ERC777 native Send functions MUST set this parameter to `true`, and backwards compatible ERC20 transfer
+    ///  functions SHOULD set this parameter to `false`.
+    function callSender(
+        address _operator,
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes _userData,
+        bytes _operatorData,
+        bool _preventLocking
+    ) private {
+        address senderImplementation = interfaceAddr(_from, "erc777_tokenHolder");
+        if (senderImplementation != 0) {
+            erc777_tokenHolder(senderImplementation).tokensToSend(
+                _operator, _from, _to, _amount, _userData, _operatorData);
+        } else if (_preventLocking) {
+            require(isRegularAddress(_from));
         }
     }
 }