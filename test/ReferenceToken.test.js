30ad8991fff74f72d9d6c2e2243170bcac26d627
Jacques Dafflon <jacques@bity.com>
2018-05-08 18:37:03 +0200

    Deploy ReferenceToken using web3.js 1.0.0

--- test/ReferenceToken.test.js
+++ test/ReferenceToken.test.js
@@ -1,16 +1,23 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+const chai = require('chai');
+const assert = chai.assert;
+chai.use(require('chai-as-promised')).should();
 const { URL } = require('url');
 const Web3 = require('web3');
 const EIP820Registry = require('eip820');
-const ReferenceToken = artifacts.require('ReferenceToken');
+const OldReferenceToken = artifacts.require('ReferenceToken');
 const utils = require('./utils');
 
 contract('ReferenceToken', function(accounts) {
   const provider = new URL(this.web3.currentProvider.host);
   provider.protocol = 'ws';
   const web3 = new Web3(provider.toString());
+  const ReferenceToken = new web3.eth.Contract(
+    OldReferenceToken.abi,
+    { data: OldReferenceToken.bytecode }
+  );
 
   let token = {
     name: 'ReferenceToken',
@@ -20,20 +27,24 @@ contract('ReferenceToken', function(accounts) {
     defaultBalance: '0',
   };
 
+  const deployContract = ReferenceToken
+    .deploy({ arguments: [
+      token.name,
+      token.symbol,
+      web3.utils.toWei(token.granularity),
+    ] });
+
   after(async function() { await web3.currentProvider.connection.close(); });
 
   beforeEach(async function() {
     let erc820Registry = await EIP820Registry.deploy(web3, accounts[0]);
     assert.ok(erc820Registry.$address);
-    let truffleToken = await ReferenceToken.new(
-      token.name,
-      token.symbol,
-      web3.utils.toWei(token.granularity),
-      { from: accounts[0], gasLimit: 800000 }
-    );
+
     // Use Web3.js 1.0
-    token.contract = new web3.eth
-      .Contract(truffleToken.abi, truffleToken.address);
+    const estimateGas = await deployContract.estimateGas();
+    token.contract = await deployContract
+      .send({ from: accounts[0], gasLimit: estimateGas });
+    assert.ok(token.contract.options.address);
 
     token.disableERC20 = async function() {
       await token.contract.methods
