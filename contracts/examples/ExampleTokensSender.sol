2c86e87fd7c23582288ba6a096f3be1309f3af1c
Jacques Dafflon <jacques@bity.com>
2018-05-09 15:08:24 +0200

    Add TokensSender tests

---
+++ contracts/examples/ExampleTokensSender.sol
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+pragma solidity 0.4.21;
+
+import { ERC820Implementer } from "eip820/contracts/ERC820Implementer.sol";
+import { ERC820ImplementerInterface } from "eip820/contracts/ERC820ImplementerInterface.sol";
+import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import { ERC777TokensSender } from "../ERC777TokensSender.sol";
+
+
+contract ExampleTokensSender is ERC820Implementer, ERC820ImplementerInterface, ERC777TokensSender, Ownable {
+
+    bool private allowTokensToSend;
+    bool public notified;
+
+    function ExampleTokensSender(bool _setInterface) public {
+        if (_setInterface) { setInterfaceImplementation("ERC777TokensSender", this); }
+        allowTokensToSend = true;
+        notified = false;
+    }
+
+    function tokensToSend(
+        address operator,   // solhint-disable no-unused-vars
+        address from,
+        address to,
+        uint amount,
+        bytes userData,
+        bytes operatorData
+    )  // solhint-enable no-unused-vars
+    public {
+        require(allowTokensToSend);
+        notified = true;
+    }
+
+    function acceptTokensToSend() public onlyOwner { allowTokensToSend = true; }
+
+    function rejectTokensToSend() public onlyOwner { allowTokensToSend = false; }
+
+    // solhint-disable-next-line no-unused-vars
+    function canImplementInterfaceForAddress(address addr, bytes32 interfaceHash) public view returns(bytes32) {
+        return ERC820_ACCEPT_MAGIC;
+    }
+
+}
--- test/utils/tokensSender.js
+++ test/utils/tokensSender.js
@@ -1,8 +1,87 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-exports.test = function(web3, accounts, tokenData) {
+const chai = require('chai');
+const assert = chai.assert;
+chai.use(require('chai-as-promised')).should();
+const utils = require('./index');
+const OldExampleTokensSender = artifacts.require('ExampleTokensSender');
+
+exports.test = function(web3, accounts, token) {
+  const ExampleTokensSender = new web3.eth.Contract(
+    OldExampleTokensSender.abi,
+    { data: OldExampleTokensSender.bytecode }
+  );
+  let sender;
   describe('TokensSender', async function() {
-    it.skip('should implement tests for "TokensSender"');
+    beforeEach(async function() {
+      await utils
+        .mintForAllAccounts(web3, accounts, token, accounts[0], '10', 100000);
+
+      sender = await ExampleTokensSender
+        .deploy({ arguments: [false] })
+        .send({ from: accounts[4], gasLimit: 4712388 });
+
+      let erc820Registry = utils.getERC820Registry(web3);
+      await erc820Registry.methods
+        .setInterfaceImplementer(
+          accounts[4],
+          web3.utils.keccak256('ERC777TokensSender'),
+          sender.options.address
+        ).send({ from: accounts[4] });
+      assert.ok(sender.options.address);
+    });
+
+    it('should notify the sender before sending tokens', async function() {
+      await utils.assertTotalSupply(web3, token, 10 * accounts.length);
+      await utils.assertBalance(web3, token, accounts[4], 10);
+      await utils.assertBalance(web3, token, accounts[5], 10);
+      await utils.assertBalance(web3, token, sender.options.address, 0);
+      assert.isFalse(await sender.methods.notified().call());
+
+      await sender.methods
+        .acceptTokensToSend()
+        .send({ gas: 300000, from: accounts[4] });
+
+      await token.contract.methods
+        .send(accounts[5], web3.utils.toWei('1.22'))
+        .send({ gas: 300000, from: accounts[4] });
+
+      await utils.getBlock(web3);
+
+      assert.isTrue(await sender.methods.notified().call());
+      await utils.assertTotalSupply(web3, token, 10 * accounts.length);
+      await utils.assertBalance(web3, token, accounts[4], 8.78);
+      await utils.assertBalance(web3, token, accounts[5], 11.22);
+      await utils.assertBalance(web3, token, sender.options.address, 0);
+    });
+
+    it('should block the sending tokens for the sender', async function() {
+      await utils.assertTotalSupply(web3, token, 10 * accounts.length);
+      await utils.assertBalance(web3, token, accounts[4], 10);
+      await utils.assertBalance(web3, token, accounts[5], 10);
+      await utils.assertBalance(web3, token, sender.options.address, 0);
+      assert.isFalse(await sender.methods.notified().call());
+
+      await sender.methods
+        .rejectTokensToSend()
+        .send({ gas: 300000, from: accounts[4] });
+
+      await token.contract.methods
+        .send(accounts[5], web3.utils.toWei('1.22'))
+        .send({ gas: 300000, from: accounts[4] })
+        .should.be.rejectedWith('revert');
+
+      await utils.getBlock(web3);
+
+      // revert will prevent setting notified to true
+      assert.isFalse(await sender.methods.notified().call());
+      await utils.assertTotalSupply(web3, token, 10 * accounts.length);
+      await utils.assertBalance(web3, token, accounts[4], 10);
+      await utils.assertBalance(web3, token, accounts[5], 10);
+      await utils.assertBalance(web3, token, sender.options.address, 0);
+    });
+
+    it.skip('should implement more tests for "TokensSender"');
   });
 };
